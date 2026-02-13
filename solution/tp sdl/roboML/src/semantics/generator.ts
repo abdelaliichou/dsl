02ec8823baaaa52812f072dab8e6bc07bf6363bf
BrokHoly <62349056+BrokHoly@users.noreply.github.com>
2026-02-13 09:19:30 +0100

    npm i -D langium-visitor & exec

---
+++ solution/tp sdl/roboML/src/semantics/generator.ts
@@ -0,0 +1,42 @@
+import type { Program } from '../language/generated/ast.js';
+import { CompositeGeneratorNode, NL, toString } from 'langium/generate';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import { extractDestinationAndName } from '../cli/cli-util.js';
+
+export function generateJavaScript(program: Program, filePath: string, destination: string | undefined): string {
+    const data = extractDestinationAndName(filePath, destination);
+    const generatedFilePath = `${path.join(data.destination, data.name)}.js`;
+
+    const fileNode = new CompositeGeneratorNode();
+    fileNode.append('"use strict";', NL, NL);
+    
+    // Generate entry function
+    if (program.entry) {
+        fileNode.append(`// Entry function: ${program.entry.returnType} entry()`, NL);
+        fileNode.append('function entry() {', NL);
+        fileNode.indent(body => {
+            body.append('// Function body here', NL);
+        });
+        fileNode.append('}', NL, NL);
+    }
+    
+    // Generate other functions
+    for (const func of program.functions) {
+        fileNode.append(`// Function: ${func.returnType} ${func.name}()`, NL);
+        fileNode.append(`function ${func.name}() {`, NL);
+        fileNode.indent(body => {
+            body.append('// Function body here', NL);
+        });
+        fileNode.append('}', NL, NL);
+    }
+    
+    fileNode.append('// Call entry function', NL);
+    fileNode.append('entry();', NL);
+
+    if (!fs.existsSync(data.destination)) {
+        fs.mkdirSync(data.destination, { recursive: true });
+    }
+    fs.writeFileSync(generatedFilePath, toString(fileNode));
+    return generatedFilePath;
+}
